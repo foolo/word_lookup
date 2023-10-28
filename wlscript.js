b2f0a7c8b6ba88fa3d82947136f52a778c581264
Olof Andersson <olof.andersson@gmail.com>
2023-10-28 18:46:42 +0200

    init

---
+++ wlscript.js
@@ -0,0 +1,47 @@
+// @ts-check
+
+function initPage() {
+	// todo mobile/desktop
+	console.log("initPage");
+}
+
+/**
+ * @returns {String} The text in the source word input box
+ */
+function getSourceText() {
+	let sourceWordInput = document.getElementById("sourceWordInput");
+	return (sourceWordInput instanceof HTMLInputElement) ? sourceWordInput.value : "";
+}
+
+/**
+ * @returns {boolean}
+ */
+function getIsMobile() {
+	const url = new URL(location.href)
+	const platform = url.searchParams.get("platform")?.toLowerCase();
+	return platform === "mobile";
+}
+
+
+function searchWiktionary() {
+	const sourceText = getSourceText();
+	const url = getIsMobile()
+		? `https://en.m.wiktionary.org/wiki/${sourceText}#Spanish`
+		: `https://en.wiktionary.org/wiki/${sourceText}#Spanish`;
+	window.open(url, "_blank");
+}
+
+function searchSpanishDict() {
+	const url = `https://www.spanishdict.com/translate/${getSourceText()}`;
+	window.open(url, "_blank");
+}
+
+function searchGoogleTranslateToEnglish() {
+	const url = `https://translate.google.se/?sl=es&tl=en&text=${getSourceText()}&op=translate`;
+	window.open(url, "_blank");
+}
+
+function searchGoogleTranslateToSwedish() {
+	const url = `https://translate.google.se/?sl=es&tl=sv&text=${getSourceText()}&op=translate`;
+	window.open(url, "_blank");
+}
